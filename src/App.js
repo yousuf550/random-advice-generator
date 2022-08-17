802b3e041f25267a1ba223d863db7587f2f7ec70
FATIK AHMED KHAN <yousuf.hanif550@gmail.com>
2022-08-17 23:43:07 +0500

    Update app.js & app.css
    Added fetch api functionality using axios  & design UI using custom css

--- src/App.js
+++ src/App.js
@@ -29,6 +29,9 @@ class App extends React.Component {
       <div className="app">
         <div className="card">
           <h1 className="heading">{advice}</h1>;
+          <button className="button" onClick={this.fetchAdvice}>
+            GIVE ME ADVICE!
+          </button>
         </div>
       </div>
     );
