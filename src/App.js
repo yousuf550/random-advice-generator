ec273c61e46e52e94be92549bb58a0a79706f03e
FATIK AHMED KHAN <yousuf.hanif550@gmail.com>
2022-08-17 23:46:12 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends React.Component {
     return (
       <div className="app">
         <div className="card">
-          <h1 className="heading">{advice}</h1>;
+          <h1 className="heading">"{advice}"</h1>;
           <button className="button" onClick={this.fetchAdvice}>
             GIVE ME ADVICE!
           </button>
